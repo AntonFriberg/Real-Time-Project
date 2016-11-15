6371807708980222ff59078728406b0bc3ce834f
AntonFriberg <anton.friberg93@gmail.com>
2016-11-15 20:51:58 +0100

    Add methods to CameraHandler and ServerSend following given design

--- src/skeleton/server/CameraHandler.java
+++ src/skeleton/server/CameraHandler.java
@@ -3,5 +3,28 @@ package skeleton.server;
 /**
  * Created by anton on 11/15/16.
  */
-public class CameraHandler {
+public class CameraHandler extends Thread{
+    /**
+     * Periodic thread that takes images according to set settings. The images,
+     * timestamp and current settings are carried out inside the monitor in
+     * order to maintain thread-safe interaction.
+     */
+
+    private CameraMonitor cm;
+    private int period = 1000; // wake thread every second
+
+    public CameraHandler(CameraMonitor cm) {
+        this.cm = cm;
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                sleep(period);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //cm.takeImage();
+        }
+    }
 }
--- src/skeleton/server/ServerSend.java
+++ src/skeleton/server/ServerSend.java
@@ -3,5 +3,18 @@ package skeleton.server;
 /**
  * Created by anton on 11/15/16.
  */
-public class ServerSend {
+public class ServerSend extends Thread{
+    /**
+     * Packages the image data, timestamp and current mode into an ImageEvent and sends it
+     * over the network via byte array over standardized protocol. There is one send
+     * thread for each camera.
+     */
+
+    private CameraMonitor cm;
+
+    public ServerSend(CameraMonitor cm) {
+        while (true) {
+
+        }
+    }
 }
