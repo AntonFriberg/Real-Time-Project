13440c9778e0b73a8156e78c151e2511c5a0d93e
Anton Friberg <anton.friberg93@gmail.com>
2016-11-28 18:12:29 +0100

    Change to proxycamera and add gitignore

--- src/skeleton/demo/SocketTest.java
+++ src/skeleton/demo/SocketTest.java
@@ -1,5 +1,6 @@
 package skeleton.demo;
 
+import skeleton.client.CameraController;
 import skeleton.client.ClientMonitor;
 import skeleton.client.ClientReceive;
 import skeleton.server.CameraMonitor;
@@ -9,11 +10,8 @@ public class SocketTest {
 
     public static void main(String[] args) {
         CameraMonitor cam = new CameraMonitor(6077);
-        ClientMonitor cm = new ClientMonitor();
         ServerSend server = new ServerSend(6077, "localhost", cam);
-        ClientReceive client = new ClientReceive("localhost", 6077, cm);
-
         server.start();
-        client.start();
+        CameraController.main(new String[]{"localhost","6077"});
     }
 }
