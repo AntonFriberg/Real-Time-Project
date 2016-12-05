ce6b58a333aad98444d01dcd85918ce0b2ab3a6d
Anton Friberg <anton.friberg93@gmail.com>
2016-12-05 18:11:09 +0100

    SocketTest for 3 proxy cameras

--- src/skeleton/demo/SocketTest.java
+++ src/skeleton/demo/SocketTest.java
@@ -9,10 +9,11 @@ public class SocketTest {
 
     public static void main(String[] args) {
      
-    	int sendPorts[] = {6077, 6080};
-        int receivePorts[] = {6078, 6081};
+    	int sendPorts[] = {6077, 6080, 6090};
+        int receivePorts[] = {6078, 6081, 6091};
         Server cam1 = new Server("argus-1.student.lth.se", sendPorts[0], receivePorts[0]);
         Server cam2 = new Server("argus-2.student.lth.se", sendPorts[1], receivePorts[1]);
+        Server cam3 = new Server("argus-3.student.lth.se", sendPorts[2], receivePorts[2]);
         cam1.start();
         cam2.start();
         
